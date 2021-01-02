b5a36e2e6d3d58e506c2f9f656c1defee5faa09c
Brian Farrell <farrelb6@tcd.ie>
2021-01-02 20:12:01 +0000

    Added vuetify and image fetching

--- github-visualisation/src/components/Visualiser.vue
+++ github-visualisation/src/components/Visualiser.vue
@@ -1,10 +1,14 @@
 <template> 
     <div id="Visualiser">
+      <v-container>
       <h1> GitHub API Visualiser </h1>
       <input v-model="message" placeholder="userName">
       <p>Search GitHub for {{ message }} ?</p>  
       <button v-on:click="searchBtn">Yes!</button>
       <p>Searching for {{searchTerm}}</p>
+
+      <v-img max-width="500px" :src = avatar> </v-img>
+      </v-container>
       <div>  
         {{info}}
       </div>
@@ -24,12 +28,21 @@ export default {
           authorization: "token " + process.env.VUE_APP_API_KEY
         }
       })
-      .then(respone => (this.info = respone))
-    }
-  },
-  data () {
-    return{message: "", searchTerm:"", info: null
-    }
-  } 
+      .then(respone => {
+      this.info = respone
+      this.getData()
+      })
+    },
+    getData() {
+      this.avatar = this.info.data.avatar_url
+      console.log(this.avatar)    
+  }
+  }, 
+  data: () => ({
+      message: "", 
+      searchTerm:"", 
+      info: null,
+      avatar:""
+  })
 }
 </script>
