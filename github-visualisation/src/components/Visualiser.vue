12faa2ad0584091b81c83dfb5573e21273c98e6f
Brian Farrell <farrelb6@tcd.ie>
2021-01-03 17:03:33 +0000

    Added graph of language commits

--- github-visualisation/src/components/Visualiser.vue
+++ github-visualisation/src/components/Visualiser.vue
@@ -11,12 +11,14 @@
       </v-card-title>
       <v-img maxwidth="200px" :src="userCard"> </v-img>
       <div>
+        <p> Top Languages by Number of Repos </p>
         <column-chart :data="languages"></column-chart>
       </div>
-      </v-container>
-      <div>  
-        {{info}}
+      <div>
+        <p> Top Languages by Number of commits </p>
+        <column-chart :data="commits"></column-chart>
       </div>
+      </v-container>
     </div>
 </template>
 
@@ -28,8 +30,7 @@ export default {
    methods: {
     searchBtn : function() {
       this.searchTerm = this.message
-      this.getLanguageData()
-      axios.get(`https://api.github.com/users/${this.searchTerm}`, {
+        axios.get(`https://api.github.com/users/${this.searchTerm}`, {
         headers: {
           authorization: "token " + process.env.VUE_APP_API_KEY
         }
@@ -38,6 +39,7 @@ export default {
       this.info = respone
       this.getData()
       this.getLanguageData()
+      
       })
     },
     getData() {
@@ -46,12 +48,7 @@ export default {
       console.log(this.info.data.repos_url)    
     },
     getLanguageData() {
-      this.languages  = []
-      this.languages.push(["A", 3])
-      this.languages.push(["b", 2])
-      this.languages.push(["c", 1])
-
-       axios.get(`https://api.github.com/users/${this.searchTerm}/repos`, {
+       axios.get(`https://api.github.com/users/${this.searchTerm}/repos?per_page=100`, {
          headers: {
            authorization: "token " + process.env.VUE_APP_API_KEY
         }
@@ -59,9 +56,11 @@ export default {
          .then(respone => {
            this.repoInfo = respone
            this.languages = []
+          
           for (let i = 0; i < this.repoInfo.data.length; i++) {
           if(!this.languages.some(row => row.includes(this.repoInfo.data[i].language)))
           {
+            if(this.repoInfo.data[i].language != null)
             this.languages.push([this.repoInfo.data[i].language,1])
           }
           else{
@@ -74,9 +73,51 @@ export default {
             }
           }
         }
-        })
-  }
+        this.getCommitData()
+      })
   },
+  getCommitData() {
+        this.commits = []
+        for (let i = 0; i < this.repoInfo.data.length; i++) {
+          axios.get(`https://api.github.com/repos/${this.searchTerm}/${this.repoInfo.data[i].name}/stats/contributors`, {
+            headers: {
+            authorization: "token " + process.env.VUE_APP_API_KEY
+        }
+       })
+       .then (response => {
+         this.currentRepoCommits = response
+         
+         if(this.repoInfo.data[i].language != null){
+          if(!this.commits.some(row => row.includes(this.repoInfo.data[i].language)))
+          {
+              for(let k = 0; k < this.currentRepoCommits.data.length; k++)
+              {
+                if (this.currentRepoCommits.data[k].author.login == this.searchTerm)
+                {
+                  this.commits.push([this.repoInfo.data[i].language, this.currentRepoCommits.data[k].total])
+                }
+              }     
+          }
+          else{
+            for(let j = 0; j < this.commits.length; j++)
+            {
+              if(this.commits[j][0] == this.repoInfo.data[i].language)
+              {
+                for(let k = 0; k < this.currentRepoCommits.data.length; k++)
+                {
+                  if (this.currentRepoCommits.data[k].author.login == this.searchTerm)
+                  {
+                    this.commits[j][1] = this.commits[j][1] + this.currentRepoCommits.data[k].total
+                  }
+                }     
+              }
+            }
+          }
+         }
+        })
+      }
+    }
+   },
   data: () => ({
       message: "", 
       searchTerm:"", 
@@ -85,7 +126,8 @@ export default {
       avatar:"",
       userCard:"",
       commits: [],
-      languages: []
+      languages: [],
+      currentRepoCommits:""
   })
 }
 </script>
